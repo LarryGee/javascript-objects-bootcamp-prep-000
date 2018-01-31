c38cdf442f4f845aa10868d392975584353e9baa
Larry Jr <larry.g3284@yahoo.com>
2018-01-31 20:37:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle})
 }
 
-function removeFromPlaylist(playlist, artistName){
+function deleteFromPlaylist(playlist, artistName){
   delete artistName.playlist
   return playlist;
 }
